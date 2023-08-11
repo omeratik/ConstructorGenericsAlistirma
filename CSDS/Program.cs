58521badfea55ac77bc62b381d5b107615d27412
omeratik <fb.omeratik@hotmail.com>
2023-08-11 20:03:22 +0300

    Add project files.

---
+++ CSDS/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel;
+
+namespace CSDS
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			List<string> adalar = new List<string>();
+			Console.WriteLine(adalar.Count);
+			adalar.Add("Sakız");
+		}
+
+
+	}
+	class MyList<T>
+	{
+		T[] array;
+
+		public MyList()
+		{
+			array = new T[1];
+		}
+
+		public void Add(T item)
+		{
+
+		}
+
+		public int Count
+		{
+			get { return array.Length; }
+		}
+	}
+}
---
+++ ConstructorGenericsAlistirma/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace ConstructorGenericsAlistirma
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Customer customer = new Customer { Id=1, FirstName = "Ömer", LastName= "Atik", City = "Bursa"}; //{} parametre değildir.
+
+            Customer customer3 = new Customer(); //Yukarıdaki yazım şekli ile aynıdır.
+            customer3.Id = 3;
+            
+            
+            
+            
+            Customer customer2 = new Customer ( 2, "Ahmet", "Atik", "Bursa"  );
+
+            
+            
+            Console.WriteLine(customer2.FirstName);
+            Console.WriteLine(customer2.LastName);
+        }
+
+	}
+	class Customer
+	{
+        public Customer()
+        {
+                
+        }
+
+        public Customer(int id, string fistName, string lastName, string city)
+        {
+            Id = id;
+            FirstName = fistName;
+            LastName = lastName;
+            City = city;
+        }
+
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName  { get; set; }
+        public string City  { get; set; }
+    }
+}
